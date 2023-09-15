2b4b044941b82bdf1029b58cd2105419aef89b2a
Luis Paredes <lparedesr@fi.uba.ar>
2023-09-15 10:40:36 -0300

    Comieno de la tarea

---
+++ tarea/src/main.rs
@@ -0,0 +1,12 @@
+// Realizar un pequeño idle game
+
+// * Algunas entidades "extraeran" (generaran aleatoriamente) oro
+// * Algunas entidades podran convertir oro en recursos (a gusto)
+// * Otras entidades podran convertir combinaciones de recursos en + oro
+// * Otras entidades podran solamente consumir recursos
+// * Periodicamente se reporta por pantalla el nivel de recursos y oro
+
+
+fn main() {
+    println!("Hello, world!");
+}
