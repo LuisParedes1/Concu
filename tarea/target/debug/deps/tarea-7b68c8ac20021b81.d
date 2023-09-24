fc71cbaa9346d0d4dc1c84b67f1a167117201f40
luis <luisjosepr@gmail.com>
2023-09-24 10:23:29 -0300

    Estado inicial de la tarea de la semana 5

---
+++ tarea/target/debug/deps/tarea-7b68c8ac20021b81.d
@@ -0,0 +1,5 @@
+/home/luis/Documents/materias/concurrentes/practica_codigo/Concu/tarea/target/debug/deps/tarea-7b68c8ac20021b81.rmeta: src/main.rs
+
+/home/luis/Documents/materias/concurrentes/practica_codigo/Concu/tarea/target/debug/deps/tarea-7b68c8ac20021b81.d: src/main.rs
+
+src/main.rs:
