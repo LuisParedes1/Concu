30f810e8a294d26103e74a3b2a56cc3af509efa0
Luis Paredes <lparedesr@fi.uba.ar>
2023-09-25 12:26:24 -0300

    Update: Se agrego un semaforo "habitacion" para evitar deadlock

--- tarea/src/main.rs
+++ tarea/src/main.rs
@@ -1,20 +1,21 @@
-use core::num;
-use std::{sync::Arc, thread, vec};
+use std::{sync::Arc, thread, time::Duration, vec};
 use std_semaphore::Semaphore;
 
-fn eat() {
-    println!("Filosofo esta comiendo");
-    thread::sleep_ms(1000);
+fn eat(i: usize) {
+    println!("Filosofo {i} esta comiendo");
+    thread::sleep(Duration::from_millis(1000));
 }
 
-fn think() {
-    println!("Filosofo esta pensando");
-    thread::sleep_ms(1000);
+fn think(_i: usize) {
+    //println!("Filosofo {i} esta pensando");
+    thread::sleep(Duration::from_millis(1000));
 }
 
 fn main() {
     const NUM_FILOSOFOS: usize = 5;
     let mut handles = vec![];
+
+    let habitaciones = Arc::new(Semaphore::new(4));
 
     // Los tenedores son semaforos que indican si estan usados o no
     let tenedores = (0..NUM_FILOSOFOS)
@@ -23,17 +24,23 @@ fn main() {
 
     for i in 0..NUM_FILOSOFOS {
         let arc_tenedores = tenedores.clone();
+        let arc_habitaciones = habitaciones.clone();
+
         handles.push(thread::spawn(move || {
             loop {
-                think();
+                think(i);
+
+                arc_habitaciones.acquire(); // wait
 
                 arc_tenedores[i].acquire(); // wait[i]
                 arc_tenedores[(i + 1) % 5].acquire(); // wait[i+1] == wait[(i+1) % 5]]
 
-                eat();
+                eat(i);
 
                 arc_tenedores[i].release(); // signal[i]
                 arc_tenedores[(i + 1) % 5].release(); // signal[i+1] = wait[(i+1) % 5]]
+
+                arc_habitaciones.release(); // signal
             }
         }));
     }
--- tarea/target/debug/deps/tarea-5d6fe6ec8e21752a.d
+++
@@ -1,5 +0,0 @@
-/home/luis/Documents/materias/concurrentes/practica_codigo/Concu/tarea/target/debug/deps/tarea-5d6fe6ec8e21752a.rmeta: src/main.rs
-
-/home/luis/Documents/materias/concurrentes/practica_codigo/Concu/tarea/target/debug/deps/tarea-5d6fe6ec8e21752a.d: src/main.rs
-
-src/main.rs:
