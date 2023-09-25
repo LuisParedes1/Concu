78e928e50346393ac74977cde42b1669c5ccbb2b
Luis Paredes <lparedesr@fi.uba.ar>
2023-09-25 12:19:40 -0300

    Estado inicial de la tarea

---
+++ tarea/README.md
@@ -0,0 +1,14 @@
+# Problema de los filosofos
+
+Cinco filósofos se sientan alrededor de una mesa y pasan
+su vida cenando y pensando.
+
+Cada filósofo tiene un plato de fideos y un palito chino a la
+izquierda de su plato. Para comer los fideos son
+necesarios dos palitos y cada filósofo sólo puede tomar
+los que están a su izquierda y derecha. Si cualquier
+filósofo toma un palito y el otro está ocupado, se quedará
+esperando, con el palito en la mano, hasta que pueda
+tomar el otro palito, para luego empezar a comer.
+
+[Notion](https://mis-notas.notion.site/Tarea-cbb9e56364964b03894375e12da73bae?pvs=4)
--- tarea/src/main.rs
+++ tarea/src/main.rs
@@ -1,3 +1,44 @@
+use core::num;
+use std::{sync::Arc, thread, vec};
+use std_semaphore::Semaphore;
+
+fn eat() {
+    println!("Filosofo esta comiendo");
+    thread::sleep_ms(1000);
+}
+
+fn think() {
+    println!("Filosofo esta pensando");
+    thread::sleep_ms(1000);
+}
+
 fn main() {
-    println!("Hello, world!");
+    const NUM_FILOSOFOS: usize = 5;
+    let mut handles = vec![];
+
+    // Los tenedores son semaforos que indican si estan usados o no
+    let tenedores = (0..NUM_FILOSOFOS)
+        .map(|_| Arc::new(Semaphore::new(1)))
+        .collect::<Vec<_>>();
+
+    for i in 0..NUM_FILOSOFOS {
+        let arc_tenedores = tenedores.clone();
+        handles.push(thread::spawn(move || {
+            loop {
+                think();
+
+                arc_tenedores[i].acquire(); // wait[i]
+                arc_tenedores[(i + 1) % 5].acquire(); // wait[i+1] == wait[(i+1) % 5]]
+
+                eat();
+
+                arc_tenedores[i].release(); // signal[i]
+                arc_tenedores[(i + 1) % 5].release(); // signal[i+1] = wait[(i+1) % 5]]
+            }
+        }));
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
 }
